0b75bc144afc43eb1565359822b9e4f48bdf511f
Phillip Pena <ppena941@gmail.com>
2022-05-17 20:18:24 -0400

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+	let name = 'Hello World.';
+	let items = [];
+	const handleChange = (e) => {
+		items = [...items, { text: e.target.value, id: Math.random() }];
+		e.target.value = '';
+	};
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Todos</h2>
+<input type="text" on:change={handleChange} />
+<ul>
+	{#each items as item (item.id)}
+		<li>{item.text}</li>
+	{/each}
+</ul>
