866dfead33931209a41fc40a0c63767ae4326948
Phillip Pena <ppena941@gmail.com>
2022-05-17 23:42:51 -0400

    Made simple todo list

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,39 @@
 <script>
-	let name = 'Hello World.';
-	let items = [];
+	import { Todo } from './../lib/classes/todo.js';
+	import TodoItem from '../lib/components/TodoItem.svelte';
+
+	let todos = [
+		new Todo('cat', 'meow'),
+		new Todo('dog', 'woof'),
+		new Todo('duck', 'quack'),
+		new Todo('cow', 'moo')
+	];
+
+	//when text input is submitted
 	const handleChange = (e) => {
-		items = [...items, { text: e.target.value, id: Math.random() }];
+		let text = e.target.value;
+		let desc = 'default';
+		todos = [...todos, new Todo(text, desc)];
+		//reset the text input
 		e.target.value = '';
+	};
+
+	//delete todo item
+	const removeFromList = (uid) => {
+		let index = todos.findIndex((todo) => todo.uid === uid);
+		todos.splice(index, 1);
+		todos = todos;
 	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h2>Todos</h2>
-<input type="text" on:change={handleChange} />
-<ul>
-	{#each items as item (item.id)}
-		<li>{item.text}</li>
-	{/each}
-</ul>
+<div id="index" class="ml-4">
+	<h1 class="text-3xl font-bold">inbox</h1>
+	<ul>
+		{#each todos as todo (todo.uid)}
+			<li class="mb-2">
+				<TodoItem {todo} on:click={removeFromList(todo.uid)} />
+			</li>
+		{/each}
+	</ul>
+	<input type="text" class="border border-slate-600" on:change={handleChange} />
+</div>
