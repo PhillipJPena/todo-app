afc4eac5b08688748c2aa488070fa8caa1afd6e8
Phillip Pena <ppena941@gmail.com>
2022-05-27 17:27:45 -0400

    created html template

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,27 @@
 <script>
-	import MainContent from './../lib/components/MainContent.svelte';
-	import Appbar from '$lib/components/Appbar.svelte';
 	import '../app.css';
-	import Sidebar from '../lib/components/Sidebar.svelte';
 
-	let projects = ['inbox', 'today', 'tomorrow', 'next 7 days'];
+	let projects = ['Inbox', 'Chores', 'Shopping List', 'Gym Routine'];
+	let openProject = projects[0];
 </script>
 
-<Appbar />
-<div class="flex h-full">
-	<Sidebar>
-		<ul class="menu whitespace-nowrap">
-			{#each projects as project}
-				<!-- content here -->
-				<li><a>{project}</a></li>
+<div class="flex h-full flex-col p-3">
+	<header class="flex justify-center ">
+		<h1 class="text-2xl font-bold ">Todo List</h1>
+	</header>
+
+	<div class="flex flex-1 flex-col gap-3">
+		<h1 class="text-4xl font-bold">{openProject}</h1>
+		<ul class="flex h-full flex-col gap-2 rounded-lg border">
+			{#each projects as proj}
+				<li>
+					<button class="btn btn-block" on:click={() => (openProject = proj)}>{proj}</button>
+				</li>
 			{/each}
-		</ul></Sidebar
-	>
-	<MainContent />
+		</ul>
+		<div class="flex h-12 w-full justify-between rounded-lg bg-black">
+			<button class="btn btn-ghost text-white">Projects</button>
+			<button class="btn btn-ghost text-white">+Add Todo</button>
+		</div>
+	</div>
 </div>
